3405cefb920eb09f33ee44b56b735cfbb2d831b9
Mohammad Hibatullah bin Hazman <hibatullahhazman@gmail.com>
2024-04-22 08:30:08 +0800

    removed the model of player and customer code

--- app/Models/excludelist.php
+++ app/Models/excludelist.php
@@ -40,16 +40,5 @@ class excludelist extends Model
     {
         return $this->belongsTo(procedures::class, 'nrproc', 'nrproc');
     }
-    
-    public function player(): BelongsTo
-    {
-        return $this->belongsTo(procedures::class, 'idproc', 'idproc');
-    }
-
-    public function customer(): BelongsTo
-    {
-        return $this->belongsTo(procedures::class, 'idcustomer', 'idplayer');
-    }
-
 
 }
---
+++ resources/views/guideline.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+
+For first time user:
+
+1. Click HOME to display the existing list of Exxluded Automation.
+2. You can perform searching for specific information such as application number, and status.
+3. The delete function was available in the same list which you need to click the delete link on the status.
+
+
+@endsection
+
+@section('heading','Add New Record')
